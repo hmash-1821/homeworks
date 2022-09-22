ad22152822c4864521f87fa091b7b4e4b5fc7e1e
hmash-1821 <plaguekilla301@gmail.com>
2022-09-22 22:48:10 +0400

    second commit

---
+++ day2/lesson2.py
@@ -0,0 +1,55 @@
+#name = "temo"
+
+#print(len(name)) #same as print(4), Len  (..counts name..)  Lenght
+          #4  
+
+#my_sentence = """გამარჯობა, შეგახსენებთ რომ სწავლას იწყებს 19 სექტემბერს,
+#გტხოვთ არ გააცდინოთ"""  #dots needed for longer text
+
+#if 10<5: #10<5 is false and 10>5 is true
+    #print("hello")
+    #with tab we made indendation
+
+
+#full_name = "temo maghlakeldize"
+#print((len)(full_name))
+
+
+#every string conside of string
+#print(full_name[11])
+#position = index სინონიმებია
+
+
+#print("k" not in full_name) #amit vigebt tu ra aso gvxvdeba sityvashi 
+#print("z" in full_name) #amit ki vigebt ra aso ar gvxvdeba sityvashi
+
+full_name = "temo maghlakeldize"
+# print(full_name[2:7:2]) #this is slice anu gachra
+# print(full_name[2:7]) 
+# #start:finish:step
+
+
+# #homework
+# print(full_name[:5])
+# print(full_name[5:])
+
+# print(full_name[-1]+"abc")
+# print(full_name[-1:-4:-1])
+
+#სტრინგებს აქვთ მეთოდები
+# 1) სტრინგ. upper()
+# 2) Len(სტრინგ)
+
+#print(full_name.upper()) #upper aris mis win yvela simbolos gazrda
+
+name= "Temo Maghlakeldize "
+#print(name.lower()) #lower apataravebs
+
+# name.strip()
+
+# print(name)
+
+#print(name.strip())
+
+print(name.replace("a", "#"))
+print(name.replace("e", "$"))
